c393129a6ba19b4336e151f041c68b2308508775
Stefan Bürk <stefan@buerk.tech>
2022-08-23 14:43:42 +0200

    [TASK] Add backend skin override to make some more sense visually

--- ext_tables.php
+++ ext_tables.php
@@ -6,6 +6,9 @@ defined('TYPO3') || die();
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php']['drawHeaderHook']['gridelements'] = \EHAERER\PasteReference\Hooks\PageLayoutController::class . '->drawHeaderHook';
 
+    $GLOBALS['TBE_STYLES']['skins']['paste_reference']['name'] = 'paste_reference';
+    $GLOBALS['TBE_STYLES']['skins']['paste_reference']['stylesheetDirectories']['paste_reference_structure'] = 'EXT:paste_reference/Resources/Public/Backend/Css/Skin/';
+
     $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1487270761] = \EHAERER\PasteReference\ContextMenu\ItemProvider::class;
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;

b8686a30d1f6941b80793c2bc2fc11cd0fbd21a3
Stefan Bürk <stefan@buerk.tech>
2022-08-23 11:22:47 +0200

    [!!!][TASK] Removed outdated PageRenderer hook
    Outdated PageRenderer hook is removed. Features original
    provided by this hook will be later reintroduced in the
    modern and future proof way to restore feature set.

--- ext_tables.php
+++ ext_tables.php
@@ -4,10 +4,6 @@ defined('TYPO3') || die();
 
 (static function () {
 
-    if (TYPO3_MODE === 'BE') {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] = 'EHAERER\\PasteReference\\Hooks\\PageRenderer->addJSCSS';
-    }
-
     $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1487270761] = \EHAERER\PasteReference\ContextMenu\ItemProvider::class;
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;

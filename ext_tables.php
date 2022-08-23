bcd1025ae65e546aba4a099449cbfc309df51345
Stefan Bürk <stefan@buerk.tech>
2022-08-23 13:56:00 +0200

    [TASK] Register `disableCopyFromPageButton` as UserSetting
    This change registers `disableCopyFromPageButton`
    in the correct way, so it is properly shown in the
    user profile settings.

--- ext_tables.php
+++ ext_tables.php
@@ -4,9 +4,20 @@ defined('TYPO3') || die();
 
 (static function () {
 
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/db_layout.php']['drawHeaderHook']['gridelements'] = \EHAERER\PasteReference\Hooks\PageLayoutController::class . '->drawHeaderHook';
+
     $GLOBALS['TYPO3_CONF_VARS']['BE']['ContextMenu']['ItemProviders'][1487270761] = \EHAERER\PasteReference\ContextMenu\ItemProvider::class;
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['moveRecordClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;
 
+    $GLOBALS['TYPO3_USER_SETTINGS']['columns']['disableCopyFromPageButton'] = [
+        'type'  => 'check',
+        'label' => 'LLL:EXT:paste_reference/Resources/Private/Language/locallang.xlf:disableCopyFromPageButton',
+    ];
+
+    $GLOBALS['TYPO3_USER_SETTINGS']['showitem'] .= ',
+    --div--;LLL:EXT:paste_reference/Resources/Private/Language/locallang_db.xlf:pasteReference,
+        disableCopyFromPageButton
+        ';
 })();

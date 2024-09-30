50cbb4a3c9a21818d882266640f00e92b7bea3ae
Stefan Bürk <stefan@buerk.tech>
2024-09-30 22:56:40 +0200

    [BUGFIX] Avoid undefined array key `showitem`
    It is recommended to extending global user settings
    within the extension `ext_tables.php` file looking
    into the documentation [1].
    
    Mostly correct, adding the custom user setting field
    is done manually not really taking care if the showitem
    array key already exists or not.
    
    TYPO3 provides a safe way for extension authors to do that
    with `ExtensionManagementUtility::addFieldsToUserSettings`
    and uses this now to avoid the
    
      1) /var/www/html/ext_tables.php:16
      Undefined array key "showitem"
    
      Triggered by:
    
      * EHAERER\PasteReference\Tests\Unit\DummyTest::extensionLoaded
        /var/www/html/Tests/Functional/DummyTest.php:40
    
    error revealed by the recently added dummy functional test.
    
    That method would allow to easily controll adding the custom
    setting before or after other settings, but is left out for
    now to keep the current setting.
    
    [1] https://docs.typo3.org/m/typo3/reference-coreapi/12.4/en-us/Configuration/UserSettingsConfiguration/Extending.html

--- ext_tables.php
+++ ext_tables.php
@@ -1,20 +1,23 @@
-<?php /** @noinspection PhpFullyQualifiedNameUsageInspection */
+<?php
 
 defined('TYPO3') || die();
 
+use EHAERER\PasteReference\Hooks\DataHandler;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 (static function () {
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['moveRecordClass'][] = \EHAERER\PasteReference\Hooks\DataHandler::class;
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = DataHandler::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = DataHandler::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['moveRecordClass'][] = DataHandler::class;
 
     $GLOBALS['TYPO3_USER_SETTINGS']['columns']['disableCopyFromPageButton'] = [
         'type' => 'check',
         'label' => 'LLL:EXT:paste_reference/Resources/Private/Language/locallang.xlf:disableCopyFromPageButton',
     ];
 
-    $GLOBALS['TYPO3_USER_SETTINGS']['showitem'] .= ',
-    --div--;LLL:EXT:paste_reference/Resources/Private/Language/locallang_db.xlf:pasteReference,
-        disableCopyFromPageButton
-        ';
+    ExtensionManagementUtility::addFieldsToUserSettings(
+        '--div--;LLL:EXT:paste_reference/Resources/Private/Language/locallang_db.xlf:pasteReference,disableCopyFromPageButton',
+    );
 })();

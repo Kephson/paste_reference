e6eb6d854eab00154a4855392eb1d6c702575c1c
Ephraim Härer <ephraim.haerer@renolit.com>
2024-11-10 12:35:17 +0100

    [TASK] removed not needed backend CSS
    Tasks:
    * removed backend CSS styling of page and content elements

--- ext_localconf.php
+++
@@ -1,9 +0,0 @@
-<?php /** @noinspection PhpFullyQualifiedNameUsageInspection */
-
-defined('TYPO3') || die();
-
-(static function () {
-
-    $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['paste_reference'] = 'EXT:paste_reference/Resources/Public/Backend/Css/Skin/';
-
-})();
